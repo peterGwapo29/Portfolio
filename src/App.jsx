03f361240d480a9ae0c7560db174a5736ae5ee8b
peterGwapo29 <peterolanolan@gmail.com>
2025-07-07 00:25:43 +0800

    adding skills

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,9 @@ function App() {
 
   return (
     <>
-
         <header className="relative bg-gray-900 h-[70px] px-6 flex items-center justify-between">
             <div className="logo absolute left-6">
-                <img src={logo} alt="logo" className="h-10" />
+                <img src={logo} alt="logo" className="h-10 drop-shadow-lg logo-bec" />
             </div>
 
             <nav className="hidden md:block mx-auto">
@@ -142,12 +141,12 @@ function App() {
                     <div className="w-full max-w-5xl">
                     <h2 className="text-2xl font-semibold mb-4 text-center">Frontend</h2><br />
                     <div className="flex flex-wrap justify-center gap-8">
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" className="h-[80px]"/>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" className="h-[80px] drop-shadow-lg" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" className="h-[80px] drop-shadow-lg" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" className="h-[80px] drop-shadow-lg" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" className="h-[80px] drop-shadow-lg" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" className="h-[80px] drop-shadow-lg" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" className="h-[80px] drop-shadow-lg"/>
                     </div>
                     </div>
 
@@ -156,22 +155,23 @@ function App() {
                     <div className="flex flex-wrap justify-center gap-8">
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg" className="h-[100px]" />
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/laravel/laravel-original-wordmark.svg" className="h-[100px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg" className="h-[80px]"/>
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg" className="h-[80px]"/>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" className="h-[80px] drop-shadow-lg" />
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg" className="h-[80px] drop-shadow-lg"/>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg" className="h-[80px] drop-shadow-lg"/>
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg" className="h-[80px] drop-shadow-lg"/>
                     </div>
                     </div>
 
                     <div className="w-full max-w-5xl">
-                    <h2 className="text-2xl font-semibold mb-4 text-center">Tools & Others</h2><br />
-                    <div className="flex flex-wrap justify-center gap-8">
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg" className="h-[100px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" className="h-[80px]" />
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original-wordmark.svg"  className="h-[80px]"/>
-                    </div>
+                        <h2 className="text-2xl font-semibold mb-4 text-center">Tools & Others</h2><br />
+                        <div className="flex flex-wrap justify-center gap-8">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg" className="h-[100px]" />
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg" className="h-[80px] drop-shadow-lg" />
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" className="h-[80px] drop-shadow-lg" />
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" className="h-[80px] drop-shadow-lg" />
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" className="h-[80px] drop-shadow-lg" />
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original-wordmark.svg"  className="h-[80px] drop-shadow-lg"/>
+                        </div>
                     </div>
 
                 </div>
@@ -187,7 +187,7 @@ function App() {
                                 <i className="fa-solid fa-code"></i>
                             </div>
                             <h1>Web Development</h1>
-                            <p className="text-sm text-gray-300 text-center">
+                            <p className="text-lg text-gray-300 text-center">
                                 I build responsive and high-performance websites using HTML, CSS, JavaScript, Tailwind, and React.
                             </p>
                         </div>
@@ -197,7 +197,7 @@ function App() {
                                 <i className="fa-solid fa-pencil-ruler"></i>
                             </div>
                             <h1>UI/UX Design</h1>
-                            <p className="text-sm text-gray-300 text-center">
+                            <p className="text-lg text-gray-300 text-center">
                                 I design intuitive and user-friendly interfaces using Figma, focusing on clean and modern design principles.
                             </p>
                         </div>
@@ -207,7 +207,7 @@ function App() {
                                 <i className="fa-solid fa-server"></i>
                             </div>
                             <h1>Backend Development</h1>
-                            <p className="text-sm text-gray-300 text-center">
+                            <p className="text-lg text-gray-300 text-center">
                                 I create and manage backend systems using PHP and Laravel with database integration (MySQL).
                             </p>
                         </div>
@@ -217,7 +217,7 @@ function App() {
                                 <i className="fa-brands fa-git-alt"></i>
                             </div>
                             <h1>Version Control</h1>
-                            <p className="text-sm text-gray-300 text-center">
+                            <p className="text-lg text-gray-300 text-center">
                                 I manage and collaborate on projects using Git and GitHub, ensuring code integrity and team efficiency.
                             </p>
                         </div>
@@ -236,7 +236,7 @@ function App() {
                         <div className="overlay-background"></div>
                         <div className="project-build relative z-10 flex flex-col items-center justify-center h-full text-center text-white p-4">
                             <h1 className="text-lg font-semibold">Club and Event Management System</h1>
-                            <p className="text-sm mb-3">Admin module only</p>
+                            <p className="text-lg mb-3">Admin module only</p>
 
                             <div className="flex gap-4 text-4xl">
                                 <i className="devicon-laravel-original colored"></i>
@@ -337,7 +337,7 @@ function App() {
         </main>
 
         <footer className='bg-gray-700 text-center flex justify-center'>
-            <p>&copy; DevByte { new Date().getFullYear() }</p>
+            <p className='drop-shadow-lg'>&copy; DevByte { new Date().getFullYear() }</p>
         </footer>
       
     </>
